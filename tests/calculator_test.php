ae0ce446cac3e75bf33d92a7a35a2e05152af43f
vidyaraghav <vidyac.raghav@gmail.com>
2014-07-30 09:31:41 +0530

    Update calculator_test.php

--- tests/calculator_test.php
+++ tests/calculator_test.php
@@ -8,7 +8,7 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
   public function testAdd(){
       $calc = new Calculator();
       $sum = $calc->add(array(2,3,4,5));
-      $this->assertEquals(14, $sum); //check if 2+3+4+5 is equal to 14
+      $this->assertEquals(15, $sum); //check if 2+3+4+5 is equal to 14
   }
 
   //test if the subtract() method in our calculator class
@@ -17,7 +17,7 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
   public function testSubtract(){
       $calc = new Calculator();
       $difference = $calc->subtract(5,2);
-      $this->assertEquals(3, $difference); //check if 5 - 2 is equal to 3
+      $this->assertEquals(4, $difference); //check if 5 - 2 is equal to 3
   }
 
   //test if the multiply() method in our calculator class
@@ -26,7 +26,7 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
   public function testMultiply(){
       $calc = new Calculator();
       $product = $calc->multiply(array(1,3,5,6));
-      $this->assertEquals(90, $product); //check if 1*3*5*6 is equal to 90
+      $this->assertEquals(91, $product); //check if 1*3*5*6 is equal to 90
   }
 
   //test if the divide() method in our calculator class
@@ -35,7 +35,7 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
   public function testDivide(){
       $calc = new Calculator();
       $quotient = $calc->divide(10,2);
-      $this->assertEquals(5, $quotient); //check if 10/2 is equal to 5
+      $this->assertEquals(6, $quotient); //check if 10/2 is equal to 5
   }
 }
 ?>
