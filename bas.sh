a9bd1740964fc731dabda8110ce866b951d0429d
Chandrakanth <chandrakanth@shippable.com>
2015-02-16 14:27:52 +0530

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,5 +1,8 @@
+git --version
 version=`git --version | awk '{print $3}'`
+echo $version
 version=( ${version//./ } )
+echo $version
 if [ "${version[0]}" -ge "1" ] && [ "${version[1]}" -gt "8" ]; then
   echo "Compatible"
 fi;
