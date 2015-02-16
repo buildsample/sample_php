75aebf9dbd6aee5766ffee6fea859c35b538ee28
Chandrakanth <chandrakanth@shippable.com>
2015-02-16 14:35:19 +0530

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -6,4 +6,6 @@ echo ${version[0]}
 echo ${version[1]}
 if [ "${version[0]}" -gt "1" ] || ( [ "${version[0]}" -eq "1" ] && [ "${version[1]}" -gt "8" ] ); then
   echo "Compatible"
+else 
+  echo "Not compatible"
 fi;
